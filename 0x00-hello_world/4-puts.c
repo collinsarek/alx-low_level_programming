5f3ef992983558fc695c9f1a4616c93b84c217a8
Ayobami Arewa <arewacollins@gmail.com>
2023-08-14 18:53:10 -0700

    hello c world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-        puts("\"programming is like building a multilingua puzzle");
+        puts("\"programming is like building a multilingual puzzle");
 	
 	return (0);
 }
